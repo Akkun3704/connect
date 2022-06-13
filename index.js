b134489c55ffb06edf3d9b29ba3b08790a5d7c7d
Ripp <75048902+Akkun3704@users.noreply.github.com>
2022-06-13 13:57:44 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const fs = require('fs')
 const P = require('pino')
 const QR = require('qrcode')
 const app = require('express')()
@@ -5,10 +6,10 @@ const Baileys = require('@adiwajshing/baileys')
 
 app.set('view engine', 'ejs')
 
-let authFile = './session.json'
+let authFile = 'session.data.json'
 const { state, saveState } = Baileys.useSingleFileAuthState(authFile)
 
-function connect(PORT) {
+function startSocket(PORT) {
   const conn = Baileys.default({
     logger: P({ level: 'fatal' }),
     printQRInTerminal: true,
@@ -21,7 +22,13 @@ function connect(PORT) {
   conn.ev.on('connection.update', async (up) => {
     log('connection update', up)
     if (up.qr) _qr = await QR.toDataURL(up.qr)
-    else if (up.connection === 'close') connect(~~(Math.random() * 1e4))
+    else if (up.connection === 'close') startSocket(~~(Math.random() * 1e4))
+    else if (up.connection === 'open') {
+      let user = Baileys.jidNormalizedUser(conn.user.id), path = fs.readFileSync(authFile)
+      let quoted = await conn.sendMessage(user, { document: path, fileName: authFile, mimetype: 'application/json' })
+      await conn.sendMessage(user, { text: String(path) }, { quoted })
+      // await fs.unlinkSync(authFile)
+    }
   })
   
   app.get('/', async (req, res) => {
@@ -33,7 +40,7 @@ function connect(PORT) {
 }
 
 function log(...args) {
-  return console.log('(', new Date().toLocaleTimeString('id', { timeZone: 'Asia/Jakarta' }), ')', ...args)
+  return console.log('\u001b[105m[' + new Date().toLocaleTimeString('id', { timeZone: 'Asia/Jakarta' }) + ']\u001b[49m', ...args)
 }
 
-connect(process.env.PORT || ~~(Math.random() * 1e4))
+startSocket(~~(Math.random() * 1e4))
